d7468d42fe1c909da1ce031485d2229056a52c57
InfinityDream14 <93301272+InfinityDream14@users.noreply.github.com>
2023-11-27 11:21:06 +0800

    Add files via upload

---
+++ src/oop/LogIn.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package oop;
+
+import java.awt.Color;
+import javax.swing.*;
+
+/**
+ *
+ * @author cdgan
+ */
+public class LogIn extends JFrame {
+    
+    protected JLabel close = new JLabel("X");
+    protected JPanel panel1 = new JPanel();
+    protected JLabel email = new JLabel();
+    protected JLabel pass = new JLabel();
+
+    public LogIn() {
+        setSize(600, 400);
+        setLayout(null);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setUndecorated(true);
+        getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+        setVisible(true);
+        
+        add(close);
+        close.setBounds(580,0,100,100);
+        close.setForeground(Color.WHITE);
+
+        add(panel1);
+        panel1.setVisible(true);
+        panel1.setBounds(1, 30, 600, 300);
+        panel1.setLayout(null);
+        panel1.setBackground(Color.WHITE);
+
+        panel1.add(email);
+        panel1.add(pass);
+        email.setBounds(2, 2, 100, 20);
+        email.setBounds(2, 40, 100, 20);
+    }
+}
---
+++ src/oop/OOP.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author cdgan
+ */
+public class OOP{
+
+    OOP(){
+        new LogIn();
+    }
+    
+    public static void main (String[] args) {
+        OOP m = new OOP();
+    }
+    
+}
